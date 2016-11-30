ea7143ebb37ea62fd4a3a6d7d1961d99b9b3a280
tranquiliza <daniel.olsen.server@gmail.com>
2016-11-30 01:18:24 +0100

    Updates

--- WebApplication/Index_OLD.php
+++ WebApplication/Index_OLD.php
@@ -19,8 +19,7 @@
 	</style>
 </head>
 <body>
-
-	<table style="width:100%">
+	<table style="width:80%" align="center">
 		<tr>
 			<th>ID</th>
 			<th>Opgave</th> 
@@ -28,49 +27,61 @@
 			<th>Forventet Start</th> 
 			<th>Personale</th> 
 			<th>Pris</th> 
-			<th>Status</th> 
+			<th>Igang</th> <!-- Placeholder -->
+			<th>Standby</th> <!-- Placeholder -->
+			<th>Færdig</th> <!-- Placeholder -->
+			<th>Afventer Godk.</th> <!-- Placeholder -->
 			<th>Frist</th>
 			<th>Afdeling</th>
 		</tr>
 	<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "dalle123";
-	$dbname = "ProjectManager";
+		$servername = "localhost";
+		$username = "root";
+		$password = "dalle123";
+		$dbname = "ProjectManager";
 
-	// Create connection
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	// Check connection
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
-	
-	//Execute SQL:
-	$sql = "SELECT * FROM Opgaver";
-	$result = $conn->query($sql);
-	
-	if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-		echo "
-			<tr>
-				<td>". $row["OpgaveID"]."</td>
-				<td>". $row["OpgaveNavn"]."</td>
-				<td>". $row["Action"]."</td>
-				<td>". $row["Forventet Start"]."</td>
-				<td>". $row["Personale"]."</td>
-				<td>". $row["Pris"]."</td>
-				<td>". $row["Status"]."</td>
-				<td>". $row["Frist"]."</td>
-				<td>". $row["Afdeling"]."</td>
-			</tr>
-			";
-        //echo "id: " . $row["OpgaveID"]. " - Name: " . $row["OpgaveNavn"]. " " . $row["Action"]. "<br>";
-    }
-	} else {
-		//If error happens we can do stuff with that?
-	}
-	$conn->close();
+		// Create connection
+		$conn = mysqli_connect($servername, $username, $password, $dbname);
+		// Check connection
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+		}
+		
+		//Execute SQL:
+		$sql = "SELECT * FROM Opgaver";
+		$result = $conn->query($sql);
+		
+		if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "<tr>";
+			echo 	"<td>". $row["OpgaveID"]."</td>";
+			echo		"<td>". $row["OpgaveNavn"]."</td>";
+			echo		"<td>". $row["Action"]."</td>";
+			echo		"<td>". $row["Forventet Start"]."</td>";
+			echo		"<td>". $row["Personale"]."</td>";
+			if($row["Pris"] != 0)
+			{
+				echo		"<td>". $row["Pris"]."</td>";
+			}
+			else
+			{
+				echo "<td>Ingen Pris</td>";
+			}
+			echo		"<td>". $row["Status"]."</td> <!-- Placeholder -->";
+			echo		"<td>". $row["Status"]."</td> <!-- Placeholder -->";
+			echo		"<td>". $row["Status"]."</td> <!-- Placeholder -->";
+			echo		"<td>". $row["Status"]."</td> <!-- Placeholder -->";
+			echo		"<td>". $row["Frist"]."</td>";
+			echo		"<td>". $row["Afdeling"]."</td>";
+			echo		"<td>". $row["Underopgave"]."</td>";
+			echo	"</tr>";
+			//echo "id: " . $row["OpgaveID"]. " - Name: " . $row["OpgaveNavn"]. " " . $row["Action"]. "<br>";
+		}
+		} else {
+			//If error happens we can do stuff with that?
+		}
+		$conn->close();
 	?>
 	</table>
 </body>
